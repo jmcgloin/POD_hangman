7d6ae1cc61030e2a06bdb6673c3fe410e1285284
Jason McGloin <jason.mcgloin@gmail.com>
2019-12-19 12:09:16 -0500

    first commit

---
+++ hangman.js
@@ -0,0 +1,49 @@
+console.log('hangman')
+const WORD = 'christmas'
+
+/*
+
+1. christmas/holiday
+2. letters guessed
+3. word and length *
+4. wrong guesses
+5. guesses til death - countdown
+6. ignore duplicate guesses
+7. track key presses
+8. update dom on guesses
+9. word to letters to blank spaces
+10. only alpha characters
+
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+
+	document.addEventListener('keyup', onGuess)
+	const characters = WORD.split('')
+	console.log()
+	game.cloak()
+	document.getElementById('displayWord').innerText = game.displayWord.join(' ')
+
+})
+
+const game = {
+	word: WORD,
+	guesses: [],
+	incorrectGuesses: [],
+	guessesRemaining: 6,
+	correctGuesses:[],
+	displayWord: [],
+	cloak: function() {
+		this.displayWord =  '_'.repeat(this.word.length).split('')
+	}
+	// onGuess: function(guess) {
+
+	// }
+}
+
+const onGuess = (e) => {
+	if(e.keyCode < 65 || e.keyCode > 90) return
+	let letter = e.key
+	game.guessesRemaining--
+	game.guesses.push(letter)
+}
